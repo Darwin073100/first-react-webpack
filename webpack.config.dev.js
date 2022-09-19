3d26bc7307cb8c8c4fce135e5de3a1c2b297e56c
Darwin073100 <edwinegq@outlook.com>
2022-09-19 17:33:02 -0500

    optimizacion del proyecto

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,24 +1,17 @@
 const path = require('path');
 const HtmlWEbpackPlugin = require('html-webpack-plugin');
 const MIniCssExtractPLugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPLugin = require('terser-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        publicPath: "/",
     },
     resolve: {
-        extensions: ['.js', '.jsx'],
-        alias:{
-            '@components': path.resolve(__dirname, 'src/components/'),
-            '@styles':path.resolve(__dirname, 'src/styles/')
-        },
+        extensions: ['.js', '.jsx']
     },
-    mode: 'production',
+    mode: 'development',
     module: {
         rules: [
             {
@@ -54,13 +47,10 @@ module.exports = {
         new MIniCssExtractPLugin({
             filename:'[name].css',
         }),
-        new CleanWebpackPlugin(),
     ],
-    optimization:{
-        minimize: true,
-        minimizer: [
-            new CssMinimizerPlugin(),
-            new TerserPLugin(),
-        ]
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 3006
     }
 }
